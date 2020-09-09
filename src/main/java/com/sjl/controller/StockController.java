5ab48314cf6237693e4388c838110820e5248c6b
shijl <1292312279@qq.com>
2020-09-09 16:41:45 +0800

    加入令牌桶算法进行限流处理
    问题：没有隐藏秒杀接口 可能其他人恶意攻击

--- src/main/java/com/sjl/controller/StockController.java
+++ src/main/java/com/sjl/controller/StockController.java
@@ -1,11 +1,14 @@
 package com.sjl.controller;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.sjl.service.StockService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/stock")
@@ -14,6 +17,8 @@ public class StockController {
 
     @Autowired
     private StockService stockService;
+
+    private RateLimiter rateLimiter = RateLimiter.create(20);//每秒可以获得令牌的数量
 
     //接收秒杀请求参数，调用业务创建订单
     @GetMapping("/kill")
@@ -33,5 +38,45 @@ public class StockController {
             return e.getMessage();
         }
 
+    }
+    //使用令牌桶进行限流处理 并使用乐观锁来处理超卖问题
+    @GetMapping("/killToken")
+    public String killToken(Integer id){
+        // 加入令牌桶算法
+        if (!rateLimiter.tryAcquire(2,TimeUnit.SECONDS)){
+            log.info("请求被抛弃：抢购失败，当前秒杀活动过于火爆，请重试！");
+            return "请求被抛弃：抢购失败，当前秒杀活动过于火爆，请重试！";
+        }
+
+        try{
+            //调用秒杀业务
+            //添加悲观锁
+            //synchronized (this){
+            int orderId = stockService.kill(id);
+            log.info("拿到订单的id为：[{}]",orderId);
+            return "订单的id为："+String.valueOf(orderId);
+            //}
+        }catch (Exception e){
+            log.info(e.getMessage());
+            return e.getMessage();
+        }
+
+    }
+
+    //令牌桶算法的使用示例
+    @GetMapping("/sale")
+    public String sale(Integer id){
+        /*令牌桶算法有两种处理机制
+        *   1. 没有获得到token的请求会一直等待直到获得token令牌*/
+        //log.info("等待的时间："+rateLimiter.acquire());
+
+        // 2.设置一个等待时间，如果在等待时间内得到token了，则处理业务，否则抛弃该请求
+        if (!rateLimiter.tryAcquire(3, TimeUnit.SECONDS)){
+            log.warn("当前请求已被限流，直接抛弃，无法进行后续业务处理");
+            return "抢购失败！";
+        }
+        log.info("处理业务........");
+
+        return "抢购成功！";
     }
 }
